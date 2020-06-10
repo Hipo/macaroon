c29bf636cd964a3870744c3b946a0c8ae704ded6
Ömer Aslan <omeremreaslan@gmail.com>
2020-06-10 15:23:44 +0300

    :fire: Remove pods folder

--- Example/Pods/Target Support Files/Pods-macaroon_Tests/Pods-macaroon_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_macaroon_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_macaroon_Tests
-@end
--- Example/Pods/Target Support Files/Tryouts/Tryouts-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Tryouts : NSObject
-@end
-@implementation PodsDummy_Tryouts
-@end
